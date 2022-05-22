75cf5743ef0e7e49ceade30f2ad3e6d5faaa388e
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-05-23 03:40:11 +0600

    transforms is  added to home page

--- src/Pages/HomePage/Component.js
+++ src/Pages/HomePage/Component.js
@@ -5,13 +5,16 @@ const Component = ({ component }) => {
     return (
         <div>
             <div class="card lg:card-side bg-base-100 shadow-xl">
-                <figure><img src={img} alt="Album" /></figure>
+                <figure
+                    className='transition ease-in-out delay-100 hover:translate-x-4 hover:scale-110 hover:bg-gray-100 duration-300'
+                ><img src={img} alt="Album" /></figure>
                 <div class="card-body">
                     <h2 class="card-title">{name}</h2>
                     <p>Price: ${price}</p>
                     <p><small>{about}</small></p>
                     <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Listen</button>
+                        <button class="btn btn-primary btn-sm transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-teal-600 hover:text-white duration-300"
+                        >Get This</button>
                     </div>
                 </div>
             </div>
